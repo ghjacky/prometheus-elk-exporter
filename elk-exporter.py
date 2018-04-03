d09994507f3c63c960b3afd5b5faf3e68c047f82
ghjacky <ldcrgmy@gmail.com>
2018-04-03 11:25:51 +0800

    非正常状态码的设置counter后无法恢复，所以需要添加正常状态码的查询并设置相应metrics值为0以示恢复

--- elk-exporter.py
+++ elk-exporter.py
@@ -25,17 +25,23 @@ def main():
     fields_nginx = ['nginx_vhost', 'nginx_request_api']
     fields_openapi = ['appId', 'url']
     querystring_nginx = '(nginx_responsecode:404 OR nginx_responsecode:403 OR nginx_responsecode:5*)'
+    querystring_nginx_ok = '(nginx_responsecode:200)'
     querystring_openapi = '(fields.logtype:openapi-access AND NOT httpCode:2*)'
+    querystring_openapi_ok = '(fields.logtype:openapi-access AND httpCode:2*)'
     query_nginx = query.Query(querystring_nginx, 'now-10s', 'now').__str__()
+    query_nginx_ok = query.Query(querystring_nginx_ok, 'now-10s', 'now').__str__()
     query_openapi = query.Query(querystring_openapi, 'now-10s', 'now').__str__()
+    query_openapi_ok = query.Query(querystring_openapi_ok, 'now-10s', 'now').__str__()
     while 1:
         for i in index_list:
             try:
                 if 'nginx-proxy' in i:
                     se = Search(i, query_nginx, fields_nginx, client)
+                    se_ok = Search(i, query_nginx_ok, fields_nginx, client)
                     data = se.getdata()
+                    data_ok = se_ok.getdata()
+                    # 非200状态码的统计条数，并设置counter
                     for item in data:
-                        print(item.values()[0])
                         # 过滤掉/api/v1/users/userid接口的请求
                         if '/api/v1/users' in item.values()[0]:
                             continue
@@ -43,14 +49,30 @@ def main():
                         request = item.get('nginx_request_api')
                         counter = data.count(item)
                         nginxErrorCount.labels(vhost, request).set(counter)
+                    # 200状态码的设置其值为0
+                    for item in data_ok:
+                        # 过滤掉/api/v1/users/userid接口的请求
+                        if '/api/v1/users' in item.values()[0]:
+                            continue
+                        vhost = item.get('nginx_vhost')
+                        request = item.get('nginx_request_api')
+                        nginxErrorCount.labels(vhost, request).set(0)
                 elif 'order-access' in i:
                     se = Search(i, query_openapi, fields_openapi, client)
+                    se_ok = Search(i, query_openapi_ok, fields_openapi, client)
                     data = se.getdata()
+                    data_ok = se_ok.getdata()
+                    # 非200状态码的统计条数，并设置counter
                     for item in data:
                         appId = item.get('appId')
                         url = item.get('url')
                         counter = data.count(item)
                         openapiErrorCount.labels(appId, url).set(counter)
+                    # 200状态码的设置其值为0
+                    for item in data_ok:
+                        appId = item.get('appId')
+                        url = item.get('url')
+                        openapiErrorCount.labels(appId, url).set(0)
             except StopIteration:
                 pass
         time.sleep(10)
